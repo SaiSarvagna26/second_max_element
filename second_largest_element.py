4bcef3933c56b33ac0aab0922864987beb021e40
SaiSarvagna26 <saisarvagnagarre2643@gmail.com>
2023-07-15 23:36:55 +0530

    first commit

---
+++ second_largest_element.py
@@ -0,0 +1,16 @@
+a=list(map(int,input().split()))
+maxi=a[0]
+list_a=[]
+for i in a:
+    if i>maxi:
+        maxi=i
+k=maxi
+for j in a:
+    if j!=k:
+        list_a=list_a+[j]
+        
+sec_max_list_a=list_a[0]
+for i in list_a:
+    if i>sec_max_list_a:
+        sec_max_list_a=i
+print(sec_max_list_a)
