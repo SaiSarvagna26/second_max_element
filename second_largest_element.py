d18e4c4dbf874bee6b3acaf86fbc9406192e6bdf
GARRE SAI SARVAGNA SADVIK <88706307+SaiSarvagna26@users.noreply.github.com>
2023-07-15 23:43:39 +0530

    second_largest_element.py

--- second_largest_element.py
+++ second_largest_element.py
@@ -1,16 +1,21 @@
 a=list(map(int,input().split()))
 maxi=a[0]
 list_a=[]
-for i in a:
-    if i>maxi:
-        maxi=i
-k=maxi
-for j in a:
-    if j!=k:
-        list_a=list_a+[j]
+
+if len(a)<=1:
+    print(-1)
+else:
+    
+    for i in a:
+        if i>maxi:
+            maxi=i
+    k=maxi
+    for j in a:
+        if j!=k:
+            list_a=list_a+[j]
         
-sec_max_list_a=list_a[0]
-for i in list_a:
-    if i>sec_max_list_a:
-        sec_max_list_a=i
-print(sec_max_list_a)
+    sec_max_list_a=list_a[0]
+    for i in list_a:
+        if i>sec_max_list_a:
+            sec_max_list_a=i
+    print(sec_max_list_a)
